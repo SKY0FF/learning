712506149dbbd65148bc4d48ab889f8c7f6beb53
SKY0FF <zaharka2901@gmail.com>
2023-03-04 16:40:28 +0300

    Create main.java

---
+++ GUI2/main.java
@@ -0,0 +1,8 @@
+package GUI2;
+
+public class main {
+    public static void main(String[] args) {
+        GUI_Sqrt app = new GUI_Sqrt();//экземпляр класса GUI
+        app.setVisible(true);//отображение окна
+    }
+}
