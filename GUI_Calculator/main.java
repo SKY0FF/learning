6e7efd09bb321ee72c9efd69a6343ae538229b89
SKY0FF <zaharka2901@gmail.com>
2023-03-10 21:07:29 +0300

    Update main.java

--- GUI_Calculator/main.java
+++ GUI_Calculator/main.java
@@ -2,7 +2,7 @@ package GUI_Calculator;
 
 public class main {
     public static void main(String[] args) {
-        GUI_Calc app = new GUI_Calc();//экземпляр класса GUI
+        GUI_Calc app = new GUI_Calc();
         app.setVisible(true);//отображение окна
     }
 }
